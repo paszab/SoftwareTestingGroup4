e80f81693cf5586765c868b199cae6eb4c0cb7f7
95.nagy.d@gmail.com <Rbableves9>
2018-09-29 15:39:38 +0200

    Project structure with modules and dependecies and empty classes are done.

---
+++ core/src/test/java/hu/uni/miskolc/iit/swtest/car/core/model/AppTest.java
@@ -0,0 +1,20 @@
+package hu.uni.miskolc.iit.swtest.car.core.model;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ dao-file/src/main/java/hu/uni/miskolc/iit/swtest/car/dao/file/App.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.iit.swtest.car.dao.file;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
